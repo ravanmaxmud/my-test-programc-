5392a295a12dbf81bd2631a0ae428977ca0fbd36
ravanmaxmud <ravanim@code.edu.az>
2022-05-26 21:46:10 +0400

    ilkc#

---
+++ DemoApp/DemoApp/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+int esqin = 20;
+int ceyhun = 20;
+int ali = 21;
+int aslan = 24;
+int elnur = 25;
+
+float topAge = esqin + ceyhun + ali + aslan + elnur;
+
+float test = topAge / 4;
+
+Console.WriteLine((topAge / 5));
+
+
+float alma = 25.5f;
+float alca = 30.4f;
+float albali = 19.8f;
+
+float topCost = alma + alca + albali;
+Console.WriteLine((topCost / 22));
+
+
+bool egercavb1dirse = false;
+
+
+
+Console.WriteLine(egercavb1dirse);
+
+float sinifa = 30;
+float sinifb = 40;
+float sinifc = 45;
+
+float vurbol = sinifa * sinifb * sinifc;
+
+Console.WriteLine(vurbol / 9);
+
+string codename = "code academy";
+Console.WriteLine(codename);
